6217f0814239ecf249cd2de64a5792532ec96057
Ara-Ellaryan <araell@mail.ru>
2020-04-12 01:20:21 +0400

    fixed mistake in massegeceratorservlet

--- src/main/java/am/home/chat/servlets/messages/MessageCreatorServlet.java
+++ src/main/java/am/home/chat/servlets/messages/MessageCreatorServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("createMessage")
+@WebServlet("/createMessage")
 public class MessageCreatorServlet extends BaseMessagesServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
